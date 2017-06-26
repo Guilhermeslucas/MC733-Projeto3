b492b90ea465e80eccc1ac332e416dc6a7d3e4b7
Vitor Falcao <vitor.falcaor@gmail.com>
2017-06-26 20:01:37 -0300

    Refining encryption

--- paillier.c
+++ paillier.c
@@ -18,6 +18,7 @@ struct key_pair {
 
 void generate_keys(struct key_pair *kp);
 void encrypt(char *buf, int buf_size, struct key_pair *kp);
+void encrypt_block(mpz_t m, struct key_pair *kp, gmp_randstate_t state, mpz_t *c);
 
 int main(int argc, char *argv[]) {
     // TODO -> Tratar entrada
@@ -36,7 +37,7 @@ int main(int argc, char *argv[]) {
 void generate_keys(struct key_pair *kp) {
     int file;
     unsigned long int seed;
-    mpz_t n, p, q, g, lambda, mi, max_num;
+    mpz_t n, p, q, max_num;
     gmp_randstate_t state;
     
     mpz_init(kp->n);
@@ -95,19 +96,19 @@ void generate_keys(struct key_pair *kp) {
     mpz_sub_ui(p, p, 1);
     mpz_sub_ui(q, q, 1);
     mpz_mul(kp->lambda, p, q);
-    mpz_invert(kp->mi, lambda, n);
+    mpz_invert(kp->mi, kp->lambda, n);
     
 }
 
 void encrypt(char *buf, int buf_size, struct key_pair *kp) {
     int file, i;
     unsigned long int seed,  n_size, blocks;
-    mpz_t m, r, n_square, e1, e2, e3, c;
+    mpz_t m, c;
     gmp_randstate_t state;
 
     n_size = mpz_sizeinbase(kp->n, 2);
     blocks = n_size/8;
-
+    
     file = open("/dev/urandom", O_RDONLY);
     if (file == -1) {
         perror("Failed to open \"/dev/urandom\"");
@@ -120,44 +121,81 @@ void encrypt(char *buf, int buf_size, struct key_pair *kp) {
     }
 
     close(file);
+    
+    gmp_randinit_default(state);
+    gmp_randseed_ui(state, seed);
 
+    i = 0;
+    while(buf_size >= blocks) {
+        // IMPORTANT!!! -> mpz_import discards leading zeroes! In decryption, must check for exact blocks. If a partial block is present, add leading zeroes to it! 
+        mpz_import(m, blocks, 1, sizeof(buf[0]), 0, 0, &buf[i]);
+        encrypt_block(m, kp, state, &c);
+        i += blocks;
+        buf_size -= blocks;
+    }
+
+    if (buf_size > 0) {
+        mpz_import(m, buf_size, 1, sizeof(buf[0]), 0, 0, &buf[i]);
+        encrypt_block(m, kp, state, &c);
+    }
+    
+    mpz_out_str(stdout, 2, m);
+    printf("\n\n");
+
+    mpz_out_str(stdout, 2, c);
+    printf("\n\n");
+}
+
+void encrypt_block(mpz_t m, struct key_pair *kp, gmp_randstate_t state, mpz_t *c) {
+    mpz_t r, n_square, e1, e2, e3;
+    
     mpz_init(r);
-    mpz_init(m);
     mpz_init(e1);
     mpz_init(e2);
     mpz_init(e3);
 
-    gmp_randinit_default(state);
-    gmp_randseed_ui(state, seed);
-    
     mpz_init(n_square);
     mpz_pow_ui(n_square, kp->n, 2);
 
-    if (buf_size < blocks) {
-        // encrypt and return
-    } else {
-        i = 0;
-        do {
-            // TODO -> Set 'm'
-            // IMPORTANT!!! -> mpz_import discards leading zeroes! In decryption, must check for exact blocks. If a partial block is present, add leading zeroes to it! 
-            mpz_import(m, blocks, 1, sizeof(buf[0]), 0, 0, &buf[i]);
-            mpz_urandomm(r, state, kp->n);
-            mpz_powm_sec(e1, kp->g, m, n_square);
-            mpz_powm_sec(e2, r, kp->n, n_square);
-            mpz_mul(e3, e1, e2);
-            mpz_mod(c, e3, n_square);
-           
-            // TODO -> Break this in two functions
-
-            i += blocks;
-            buf_size -= blocks;
-        } while(buf_size >= blocks);
-
-        if (buf_size > 0) {
-            // encrypt remaining blocks
-        }
-    }
-    
-    mpz_init(m);
-    mpz_import(m, buf_size, 1, sizeof(buf[0]), 0, 0, buf);
+    mpz_urandomm(r, state, kp->n);
+    mpz_powm_sec(e1, kp->g, m, n_square);
+    mpz_powm_sec(e2, r, kp->n, n_square);
+    mpz_mul(e3, e1, e2);
+    mpz_mod(*c, e3, n_square);
 }
+/*
+void decrypt(mpz_t c, struct key_pair *kp) {
+    int i;
+    unsigned long int n_size, blocks;
+    mpz_t m, n_square, d1, d2;
+    
+    n_size = mpz_sizeinbase(kp->n, 2);
+    blocks = n_size/8;
+    
+    /*i = 0;
+    while(buf_size >= blocks) {
+        // IMPORTANT!!! -> mpz_import discards leading zeroes! In decryption, must check for exact blocks. If a partial block is present, add leading zeroes to it! 
+        mpz_import(m, blocks, 1, sizeof(buf[0]), 0, 0, &buf[i]);
+        i += blocks;
+        buf_size -= blocks;
+    }
+
+    if (buf_size > 0) {
+        mpz_import(m, buf_size, 1, sizeof(buf[0]), 0, 0, &buf[i]);
+    }
+    mpz_init(n_square);
+    mpz_pow_ui(n_square, kp->n, 2);
+
+    mpz_powm_sec(d1, c, kp->lambda, n_square);
+    mpz_sub_ui(d2, d1, 1);
+    mpz_div
+
+    //L(c^lambda mod n_square)*mi mod n
+    L = (x - 1)/n
+    
+    mpz_out_str(stdout, 2, m);
+    printf("\n\n");
+
+    mpz_out_str(stdout, 2, c);
+    printf("\n\n");
+}*/
