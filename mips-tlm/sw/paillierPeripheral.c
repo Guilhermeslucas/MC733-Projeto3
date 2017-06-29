04b65a2ffb2eb9ff170e4f5c47a8a36eaa32aca5
Guilhermeslucas <guilherme.slucas@gmail.com>
2017-06-29 12:10:04 -0300

    adding paillierperipheral

---
+++ mips-tlm/sw/paillierPeripheral.c
@@ -0,0 +1,238 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <gmp.h>
+#include <string.h>
+
+#define PRIME_SIZE 1536  // bits
+#define LOCKADDR 0x4000000
+#define TOTALPROCS 4
+
+struct key_pair {
+    // Public key
+    mpz_t g;
+    mpz_t n;
+
+    // Private key
+    mpz_t lambda;
+    mpz_t mi;    
+};
+
+struct perInfos {
+    struct key_pair* key;
+    char* buf;
+    int buf_size;
+    mpz_t* c;
+};
+
+
+volatile int *lock = (volatile int *) LOCKADDR;
+volatile int procCount = 0;
+int id = 0;
+int waitMainProc = 1;
+
+mpz_t p, q, max_num;
+
+void acquireLock() {
+    while (*lock);
+}
+
+void releaseLock() {
+    *lock = 0;
+}
+
+void generate_keys(struct key_pair *kp);
+void encrypt(char *buf, int buf_size, struct key_pair *kp, mpz_t *c);
+void encrypt_block(mpz_t m, struct key_pair *kp, gmp_randstate_t state, mpz_t *c);
+void decrypt(mpz_t *c, struct key_pair *kp);
+
+int main(int argc, char *argv[]) {
+    // TODO -> Tratar entrada
+    struct key_pair kp;
+    mpz_t c;
+    struct per_infos*;
+    per_infos = malloc(sizeof(struct perInfos));
+
+    printf("%s\n", argv[1]);
+
+    acquireLock();
+    id = ++procCount;
+    printf("Hello world, it's processor %d\n", id);
+    releaseLock();
+
+    // if option == generate keys
+    generate_keys(&kp);
+
+    // defining structures run the code on the peripheral
+    per_infos->key = &kp;
+    strcpy(per_infos->buf, argv[1]);
+    per_infos->buf_size = strlen(argv[1]);
+    per_infos->c = &c;
+    *lock = per_infos;
+    //encrypt(argv[1], strlen(argv[1]), &kp, &c);
+    //decrypt(&c, &kp);
+    return 0;
+}
+
+
+// Generate a public/private key-pair
+// TODO -> p and q generation can be made in parallel
+void generate_keys(struct key_pair *kp) {
+    int file;
+	unsigned long int seed;
+	gmp_randstate_t state;
+
+	mpz_init(kp->n);
+	mpz_init(kp->g);
+	mpz_init(kp->lambda);
+	mpz_init(kp->mi);
+
+	if (id == 1) {
+		mpz_init2(p, PRIME_SIZE);
+		mpz_init2(q, PRIME_SIZE);
+		mpz_init(max_num);
+		mpz_ui_pow_ui(max_num, 2, 1536);
+		mpz_sub_ui(max_num, max_num, 1);
+		waitMainProc = 0;
+	} else while(waitMainProc);
+		
+	file = open("/dev/urandom", O_RDONLY);
+	if (file == -1) {
+		perror("Failed to open \"/dev/urandom\"");
+		exit(1);
+	}
+	
+	if (read(file, &seed, sizeof(unsigned long int)) == -1) {
+		perror("Failed to read random bytes");
+		exit(1);
+	}
+
+	close(file);
+	
+	gmp_randinit_default(state);
+	gmp_randseed_ui(state, seed);
+    
+	mpz_t *val;
+	
+	if (id == 1) val = &p;
+	else if (id == 2) val = &q;
+
+    // Generate one prime
+    do {
+        mpz_urandomb(*val, state, PRIME_SIZE);
+        mpz_setbit(*val, PRIME_SIZE - 1);
+        mpz_setbit(*val, 0);
+        if (mpz_probab_prime_p(*val, 31) == 0) {  // Certainly not prime
+            mpz_nextprime(*val, *val);  // TODO -> Check if I can call mpz_nextprime this way	
+            // mpz_nextprime could generate a prime greather than 2^PRIME_SIZE - 1, that's why we don't "break"
+        } else {  
+            break; 
+        }
+    } while (mpz_cmp(*val, max_num) > 0);
+
+	if (id == 1) {
+		mpz_mul(kp->n, p, q);
+		mpz_add_ui(kp->g, kp->n, 1);
+
+		mpz_sub_ui(p, p, 1);
+		mpz_sub_ui(q, q, 1);
+		mpz_mul(kp->lambda, p, q);
+
+		mpz_invert(kp->mi, kp->lambda, kp->n);
+	}
+}
+
+void encrypt(char *buf, int buf_size, struct key_pair *kp, mpz_t *c) {
+    int file;
+    unsigned long int seed;
+    mpz_t m;
+    gmp_randstate_t state;
+    
+    file = open("/dev/urandom", O_RDONLY);
+    if (file == -1) {
+        perror("Failed to open \"/dev/urandom\"");
+        exit(1);
+    }
+    
+    if (read(file, &seed, sizeof(unsigned long int)) == -1) {
+        perror("Failed to read random bytes");
+        exit(1);
+    }
+
+    close(file);
+    
+    gmp_randinit_default(state);
+    gmp_randseed_ui(state, seed);
+    
+    mpz_init(m);
+    mpz_import(m, buf_size, 1, sizeof(buf[0]), 0, 0, buf);
+   
+    if (mpz_cmp(m, kp->n) > 0) { // TODO -> Check if it must be m < n or m <= n
+        perror("m must be smaller than n");
+        exit(1);
+    }
+    printf("Message: ");
+    mpz_out_str(stdout, 16, m);
+    printf("\n\n");
+
+    encrypt_block(m, kp, state, c);
+
+    printf("Encrypted messaged: ");
+    mpz_out_str(stdout, 16, *c);
+    printf("\n\n");
+}
+
+void encrypt_block(mpz_t m, struct key_pair *kp, gmp_randstate_t state, mpz_t *c) {
+    mpz_t r, n_square, e1, e2, e3;
+   
+    mpz_init(*c);
+    mpz_init(r);
+    mpz_init(e1);
+    mpz_init(e2);
+    mpz_init(e3);
+
+    mpz_init(n_square);
+    mpz_pow_ui(n_square, kp->n, 2);
+
+    mpz_urandomm(r, state, kp->n);
+    mpz_powm_sec(e1, kp->g, m, n_square);
+    mpz_powm_sec(e2, r, kp->n, n_square);
+    mpz_mul(e3, e1, e2);
+    mpz_mod(*c, e3, n_square);
+}
+
+void decrypt(mpz_t *c, struct key_pair *kp) {
+    mpz_t n_square, d1;
+ 
+    /*
+    xp = x mod p
+    xq = x mod q
+
+    yp = xp^(dp) mod p
+    yq = xq^(dq) mod q
+
+    dp = d mod (p-1)
+    dq = d mod (q-1)
+
+    y = q*cp*yp + p*cq*yq mod n
+
+    cp = q^(-1) mod p
+    cq = p^(-1) mod q
+
+    */
+    mpz_init(n_square);
+    mpz_pow_ui(n_square, kp->n, 2);
+    
+    mpz_init(d1);
+    mpz_powm_sec(d1, *c, kp->lambda, n_square);
+    mpz_sub_ui(d1, d1, 1);
+    mpz_divexact(d1, d1, kp->n);
+
+    mpz_mul(d1, d1, kp->mi);
+    mpz_mod(d1, d1, kp->n);
+    
+    printf("Recovered messaged: ");
+    mpz_out_str(stdout, 16, d1);
+    printf("\n\n");
+}
